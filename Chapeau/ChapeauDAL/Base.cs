bf9c77acc0ab05add945a6d3b4cc5711b975c4be
MorRochben <Sjoerd-k@hotmail.nl>
2020-04-29 17:22:31 +0200

    Added sql base class

--- Chapeau/ChapeauDAL/Base.cs
+++ Chapeau/ChapeauDAL/Base.cs
@@ -12,8 +12,7 @@ namespace SomerenDAL
         private SqlConnection conn;
         public Base()
         {
-            // DO NOT FORGET TO INSERT YOUR CONNECTION STRING NAMED 'SOMEREN DATABASE' IN YOUR APP.CONFIG!!
-            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SomerenDatabase"].ConnectionString);
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ChapeauDatabase"].ConnectionString);
             adapter = new SqlDataAdapter();
         }
 
