011117922f7b448255c24763820cf8feb24f83c0
MorRochben <Sjoerd-k@hotmail.nl>
2020-05-10 16:01:12 +0200

    Added MenuItem class

---
+++ Chapeau/ChapeauModel/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    class MenuItem
+    {
+        int ID;
+        public string Name { get; private set; }
+        public string Category { get; private set; }
+        public float Price { get; private set; }
+        public string description;
+        public int stock;
+    }
+}
